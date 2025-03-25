fbee675788d0e8a41a044329538af9a944700bd2
titch <sujinlee0202@gmail.com>
2025-03-25 19:53:34 +0900

    fix: signup 버튼 텍스트 수정

--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -5,7 +5,12 @@ import { Dispatch, SetStateAction, useState } from "react";
 import { FloatLabelInput } from "../common/FloatLabelInput";
 import { Button } from "../ui/button";
 import { useMutation } from "@tanstack/react-query";
-import { InputOTP, InputOTPGroup, InputOTPSeparator, InputOTPSlot } from "../ui/input-otp";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "../ui/input-otp";
 
 interface Props {
   setView: Dispatch<SetStateAction<string>>;
@@ -71,15 +76,19 @@ export default function SignUp({ setView }: Props) {
   // TODO : 회원가입 완료 후 input state 초기화
 
   return (
-    <div className="flex flex-col gap-4">
-      <div className="pt-10 pb-6 px-10 w-full flex flex-col items-center justify-center max-w-xl border border-gray-400 bg-white gap-2">
+    <div className='flex flex-col gap-4'>
+      <div className='pt-10 pb-6 px-10 w-full flex flex-col items-center justify-center max-w-xl border border-gray-400 bg-white gap-2'>
         {/** Logo */}
-        <p className="text-2xl font-bold mb-6 ">INFLEARNGRAM</p>
+        <p className='text-2xl font-bold mb-6 '>INFLEARNGRAM</p>
 
         {/** Input Field */}
-        <div className="flex flex-col gap-4">
+        <div className='flex flex-col gap-4'>
           {confirmationRequired ? (
-            <InputOTP maxLength={6} value={otp} onChange={(value) => setOtp(value)}>
+            <InputOTP
+              maxLength={6}
+              value={otp}
+              onChange={(value) => setOtp(value)}
+            >
               <InputOTPGroup>
                 <InputOTPSlot index={0} />
                 <InputOTPSlot index={1} />
@@ -94,21 +103,42 @@ export default function SignUp({ setView }: Props) {
             </InputOTP>
           ) : (
             <>
-              <FloatLabelInput id="email" label="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-              <FloatLabelInput id="password" label="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+              <FloatLabelInput
+                id='email'
+                label='Email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <FloatLabelInput
+                id='password'
+                label='Password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </>
           )}
-          <Button className="h-10 rounded-md text-sm bg-blue-500 font-bold" onClick={handleSignup}>
-            {confirmationRequired && isPending ? "Loading..." : "회원가입"}
-            {confirmationRequired && otpIsPending ? "Loading..." : "확인"}
+          <Button
+            className='h-10 rounded-md text-sm bg-blue-500 font-bold'
+            onClick={handleSignup}
+          >
+            {confirmationRequired
+              ? otpIsPending
+                ? "Loading..."
+                : "확인"
+              : isPending
+              ? "Loading..."
+              : "회원가입"}
           </Button>
         </div>
       </div>
 
       {/** Go to Login */}
-      <div className="py-4 w-full text-center max-w-lg border border-gray-400 bg-white text-sm">
+      <div className='py-4 w-full text-center max-w-lg border border-gray-400 bg-white text-sm'>
         이미 계정이 있으신가요?{" "}
-        <button className="text-blue-500 font-bold" onClick={() => setView("SIGNIN")}>
+        <button
+          className='text-blue-500 font-bold'
+          onClick={() => setView("SIGNIN")}
+        >
           로그인하기
         </button>
       </div>
